4ddca9c3a21fb39a5dfe89130722f314934ec755
kazuki miyata <kazuki745655@gmail.com>
2016-12-25 18:44:00 +0900

    unityちゃんを使ったゲーム
    キャラクターの動かし方や衝突判定など

---
+++ Assets/DestroyWall.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class DestroyWall : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.position += new Vector3(0f, 0f, 0.2f);
+	}
+
+	//void OnCollisionEnter(Collision other){
+	void OnTriggerEnter(Collider other) {	
+		if (other.gameObject.tag == "CarTag" || other.gameObject.tag == "TrafficConeTag") {
+			Destroy (other.gameObject);
+		}
+		if (other.gameObject.tag == "CoinTag") {
+			Destroy (other.gameObject);
+		}
+	}
+}
+
